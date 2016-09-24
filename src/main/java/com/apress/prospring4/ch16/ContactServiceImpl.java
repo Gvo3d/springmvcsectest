220ee1338678293736fe8d386248c76a411a4fd3
Gvo3d <denis.yakimov89@gmail.com>
2016-09-24 19:55:51 +0300

    v.0.0.1-7

---
+++ src/main/java/com/apress/prospring4/ch16/ContactServiceImpl.java
@@ -0,0 +1,37 @@
+package com.apress.prospring4.ch16;
+
+import com.apress.prospring4.ContactService;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Repository
+@Transactional
+@Service("contactService")
+public class ContactServiceImpl implements ContactService {
+    private ContactRepository contactRepository;
+
+    @Transactional(readOnly = true)
+    public List<Contact> findAll() {
+        return Lists.newArrayList(contactRepository.findAll());
+    }
+
+    @Transactional(readOnly = true)
+    public Contact findById(Long id) {
+        return contactRepository.findOne(id);
+    }
+
+    public Contact save(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    @Autowired
+    public void setContactRepository(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+}
